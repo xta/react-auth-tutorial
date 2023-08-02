117c0610c407c8b818edc546e912a5535b35ecd3
xta <rexfeng@gmail.com>
2023-08-02 01:33:37 -0700

    use Login page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Login from '../Login/Login';
 import { Dashboard } from '../Dashboard/Dashboard';
 import { Preferences } from '../Preferences/Preferences';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
