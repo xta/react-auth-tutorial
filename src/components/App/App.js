cdefb0c278e69a83339cef44d62f0e65c3d38b3f
xta <rexfeng@gmail.com>
2023-08-02 04:10:12 -0700

    add Login Button to show the Login form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,19 @@ import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import useToken from './useToken'
 
 import Login from '../Login/Login';
+import LoginButton from '../Login/Button';
 import Logout from '../Logout/Logout';
 import { Dashboard } from '../Dashboard/Dashboard';
 import { Preferences } from '../Preferences/Preferences';
 
 function App() {
   const { token, clear, setToken } = useToken()
+  const [showLogin, setShowLogin] = useState(false)
 
-  if (!token) {
-    return <Login setToken={setToken} />
+  const toggleShowLogin = () => setShowLogin(!showLogin)
+  const loginToAccount = token => {
+    setToken(token)
+    setShowLogin(false)
   }
 
   return (
@@ -20,16 +24,18 @@ function App() {
       <BrowserRouter>
         <h1><Link to='/'>Home</Link></h1>
         <Link to='/dashboard'>Dashboard</Link> |&nbsp;
-        <Link to='/preferences'>Preferences</Link>
+        <Link to='/preferences'>Preferences</Link>&nbsp;
 
+        {(!token && <LoginButton onPress={toggleShowLogin} />)}
         {(token && <Logout clear={clear} />)}
+
+        {showLogin && !token && (<Login setToken={loginToAccount} />)}
 
         <Routes>
           <Route path="/dashboard" element={<Dashboard />}>
-
           </Route>
-          <Route path="/preferences" element={<Preferences />}>
 
+          <Route path="/preferences" element={<Preferences />}>
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/components/Login/Button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function LoginButton({ onPress }) {
+    return (
+        <button onClick={onPress}>Login</button>
+    )
+}
+
+export default LoginButton
