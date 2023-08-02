6f93ca13e6cc96ad7c04eb08f2bba80b91b437ed
xta <rexfeng@gmail.com>
2023-08-02 01:11:23 -0700

    create dashboard & preferences pages with router v6

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Dashboard } from '../Dashboard/Dashboard';
+import { Preferences } from '../Preferences/Preferences';
 
 function App() {
   return (
-    <></>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />}>
+
+          </Route>
+          <Route path="/preferences" element={<Preferences />}>
+
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
